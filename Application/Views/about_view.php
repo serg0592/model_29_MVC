abbfa2d5ca86b862d317812b1373ecd0ecb77aca
Sergey Kudelin <kudelin.work@bk.ru>
2022-05-25 19:56:17 +0300

    Removed: PHP img

--- Application/Views/about_view.php
+++ Application/Views/about_view.php
@@ -6,16 +6,9 @@
     <p>Какая-то картинка
     </p>
     <?php 
-        /*echo <<<'EOD'
+        echo <<<'EOD'
             <img src="img/Rectangle 9.1.png">
-        EOD;*/
-        $img = imagecreatetruecolor(100, 100);
-        $bg = imagecolorallocate($img, 255, 255, 255);
-        imagefill($img, 0, 0, $bg);
-        $black = imagecolorallocate($img, 0, 0, 0);
-        imageellipse($img, 10, 10, 10, 10, $black);
-        //div("Content-type: image/png");
-        imagepng($img);
+        EOD;
     ?>
 </div>
 <div class="item box5 addition">
