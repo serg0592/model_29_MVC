d0e7c009d6d2b7da66b7b46e4bb4394c9f3d4009
Sergey Kudelin <kudelin.work@bk.ru>
2022-05-27 23:46:56 +0300

    Updated: pages url

--- Application/Views/Source/menu.inc.php
+++ Application/Views/Source/menu.inc.php
@@ -1,7 +1,7 @@
 <nav>
-    <a href="index.php?url=main" style="color: #34547A;">ГЛАВНАЯ</a>
-    <a href="index.php?url=about">ОБ АВТОРЕ</a>
-    <a href="index.php?url=work">РАБОТА</a>
-    <a href="index.php?url=process">ПРОЦЕСС</a>
-    <a href="index.php?url=contacts">КОНТАКТЫ</a>
+    <a href="main" style="color: #34547A;">ГЛАВНАЯ</a>
+    <a href="about">ОБ АВТОРЕ</a>
+    <a href="work">РАБОТА</a>
+    <a href="process">ПРОЦЕСС</a>
+    <a href="contacts">КОНТАКТЫ</a>
 </nav>
